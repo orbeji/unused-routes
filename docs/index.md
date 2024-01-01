229d5831db510b0b9cf5f70cf6264d35a3dbedf8
orbeji <oriol@bernado.cat>
2024-01-01 22:24:23 +0100

    changes in extension structure

---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# UnusedRoutes
--- src/DependencyInjection/UnusedRoutesExtension.php
+++ src/DependencyInjection/UnusedRoutesExtension.php
@@ -4,12 +4,15 @@ namespace Orbeji\UnusedRoutes\DependencyInjection;
 
 use Exception;
 use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class UnusedRoutesExtension extends Extension
+class UnusedRoutesExtension extends Extension implements ConfigurationInterface
 {
     /**
      * @throws Exception
@@ -24,5 +27,26 @@ class UnusedRoutesExtension extends Extension
         foreach ($config as $key => $value) {
             $container->setParameter('unused_routes.' . $key, $value);
         }
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container): ?ConfigurationInterface
+    {
+        return $this;
+    }
+
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('unused_routes');
+
+        $rootNode = $treeBuilder->getRootNode();
+        \assert($rootNode instanceof ArrayNodeDefinition);
+
+        $rootNode
+            ->children()
+                ->scalarNode('file_path')->defaultValue('/var/log/unused_routes.log')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
     }
 }
